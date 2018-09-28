f4a951be1ae7675775feee0cf33639ee0cd82914
JMRVman <onyourmarco@gmail.com>
2018-09-28 16:40:51 -0700

    added moment.js to express generated framework

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET Hello World page. */
+router.get('/helloworld', function(req, res) {
+    res.render('helloworld', { title: 'Hello, World!' });
+});
+
+/* GET Moment page. */
+router.get('/moment', function(req, res) {
+    res.render('moment', { title: 'Date and Time' });
+});
+module.exports = router;
