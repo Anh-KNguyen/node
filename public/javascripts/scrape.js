8b7ca47425e9091eb9dbbf54c0e29af81df19103
Annnh3 <anhknguyen97@yahoo.com>
2018-10-30 15:24:42 -0700

    slightly better formatting

--- public/javascripts/scrape.js
+++ public/javascripts/scrape.js
@@ -4,7 +4,7 @@ var cheerio = require('cheerio');
 request('https://www.cpp.edu/events/#.W9jKA2hKjIW', function (error, response, html) {
   if (!error && response.statusCode == 200) {
     var $ = cheerio.load(html);
-    $('div.event').each(function(i, element){
+    $('div.padding').each(function(i, element){
       var a = $(this);
       console.log(a.text());
     });
