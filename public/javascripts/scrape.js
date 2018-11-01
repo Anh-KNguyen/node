b7838ae7d1d9179fff248f5b290d75535764aa60
Annnh3 <anhknguyen97@yahoo.com>
2018-10-31 21:36:47 -0700

    title and date into variables

--- public/javascripts/scrape.js
+++ public/javascripts/scrape.js
@@ -1,9 +1,21 @@
 var request = require('request');
 var cheerio = require('cheerio');
-
-request('https://www.cpp.edu/events/#.W9jKA2hKjIW', function (error, response, html) {
+var links = ['https://calendar.cpp.edu/MasterCalendar/RSSFeeds.aspx?data=exfsp9BbGwOa3zSOgl8KgGNgCYC75ztqqkFP6cJRnpY%3d']
+request(links[0], function (error, response, html) {
   if (!error && response.statusCode == 200) {
     var $ = cheerio.load(html);
+    $('item').each(function(i,element) {
+        var title = $(element).children('title').text();
+        title = title.replace("<![CDATA[", "").replace("]]>", "");
+        var date = $(element).children('pubDate');
+        //var description = $(element).children('description').text();
+        //description.replace("<![CDATA[", "").replace("]]>", "");
+        //console.log($(element).children('description'));
+        
+        console.log(title);
+        console.log(date.text());
+        //console.log(description);
+    });
     $('div.padding').each(function(i, element){
       var a = $(this);
       console.log(a.text());
