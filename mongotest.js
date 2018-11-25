e4832fefa6e40a98cd336cc6eb54d43916fd0338
Annnh3 <anhknguyen97@yahoo.com>
2018-11-24 23:50:13 -0800

    cpp events saves to database and displays on calendar

--- mongotest.js
+++ mongotest.js
@@ -1,22 +1,60 @@
 var mongoose = require('mongoose');
-var Event = require('models/event');
+var Event = require('./models/event');
+
+var request = require('request');
+var cheerio = require('cheerio');
+var Parser = require('rss-parser');
+var parser = new Parser();
+var dateFormat = require('dateformat');
+
+var links = ['https://calendar.cpp.edu/MasterCalendar/RSSFeeds.aspx?data=6UXt45wBkTcFIBid%2b4Gb2e2%2bDkhVSdjk'];
+
+var allRssItems = [];
+
 mongoose.connect('mongodb://localhost:27017/node',{ useNewUrlParser: true });
 
-var eventTitle = "Script Test";
-var eventStart = "2018-11-23";
+var eventTitle = "";
+var eventStart = "";
 var eventEnd = "";
 var eventURL = "";
-var eventDescription = "This works now.";
+var eventDescription = "";
 
-var newEvent = new Event({
-   title : eventTitle,
-   description : eventDescription,
-   url : eventURL,
-   eventStart : eventStart,
-   eventEnd : eventEnd
-});
+var now = new Date();
 
-Event.createEvent(newEvent, function (err, event) {
-  if (err) throw err;
-  console.log('Event created!');
-});
+  links.forEach(
+    function(link) {  
+        (async () => {
+            let feed = await parser.parseURL(links[0]);
+            console.log(feed.title);
+          
+            feed.items.forEach(item => {
+                allRssItems = item;
+
+                eventTitle = item.title;
+                eventDescription = item.content;
+                eventURL = item.link;
+
+                eventStart = dateFormat(item.pubDate, "isoDate");
+
+                var newEvent = new Event({
+                    title : eventTitle,
+                    description : eventDescription,
+                    url : eventURL,
+                    eventStart : eventStart,
+                    eventEnd : eventEnd
+
+                });
+
+                Event.createEvent(newEvent, function (err, event) {
+                    if (err) throw err;
+                    console.log('Event created!');
+
+                });
+
+
+            });
+
+        })();
+
+    });
+
