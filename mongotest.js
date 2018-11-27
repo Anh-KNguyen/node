3d91b1f29da773c6b239c7dd0d4e9feb815a2b2a
Alex Su <alexlocsu@gmail.com>
2018-11-26 16:33:31 -0800

    fixed foreach loop, runs properly with one call

--- mongotest.js
+++ mongotest.js
@@ -7,14 +7,11 @@ var Parser = require('rss-parser');
 var parser = new Parser();
 var dateFormat = require('dateformat');
 
-//var links = ['https://calendar.cpp.edu/MasterCalendar/RSSFeeds.aspx?data=exfsp9BbGwOa3zSOgl8KgGNgCYC75ztqqkFP6cJRnpY%3d']; //Ursa Minor
-//var links = ['https://calendar.cpp.edu/MasterCalendar/RSSFeeds.aspx?data=6UXt45wBkTcFIBid%2b4Gb2e2%2bDkhVSdjk']; //Orion
-//var links = ['https://calendar.cpp.edu/MasterCalendar/RSSFeeds.aspx?data=OiNeXA6LJItp%2bLkkMsbi4050X4kD6epuP4vCurdqfSD0uyOJgBAweQ%3d%3d']; //CPP events
-//var links = ['https://calendar.cpp.edu/MasterCalendar/RSSFeeds.aspx?data=E6dodmaNJ5NKdXCNLg2VPn652OiRBsgF88G3P4FwrJ8%3d']; //Centaurus
-var links = ['https://calendar.cpp.edu/MasterCalendar/RSSFeeds.aspx?data=VXLCORHhDCF9BYlLwAXiIJvPWWGYQMdCK0%2bRug%2fT9ac%3d']; //Ursa Major
-
-var allRssItems = [];
-var channelItems = [];
+var links = ['https://calendar.cpp.edu/MasterCalendar/RSSFeeds.aspx?data=exfsp9BbGwOa3zSOgl8KgGNgCYC75ztqqkFP6cJRnpY%3d', //Ursa Minor
+			'https://calendar.cpp.edu/MasterCalendar/RSSFeeds.aspx?data=6UXt45wBkTcFIBid%2b4Gb2e2%2bDkhVSdjk', //Orion
+			'https://calendar.cpp.edu/MasterCalendar/RSSFeeds.aspx?data=OiNeXA6LJItp%2bLkkMsbi4050X4kD6epuP4vCurdqfSD0uyOJgBAweQ%3d%3d', //CPP Events
+			'https://calendar.cpp.edu/MasterCalendar/RSSFeeds.aspx?data=E6dodmaNJ5NKdXCNLg2VPn652OiRBsgF88G3P4FwrJ8%3d', //Centarus
+			'https://calendar.cpp.edu/MasterCalendar/RSSFeeds.aspx?data=VXLCORHhDCF9BYlLwAXiIJvPWWGYQMdCK0%2bRug%2fT9ac%3d']; // Ursa Major
 
 mongoose.connect('mongodb://localhost:27017/node',{ useNewUrlParser: true });
 
@@ -30,11 +27,10 @@ var now = new Date();
   links.forEach(
     function(link) {  
         (async () => {
-            let feed = await parser.parseURL(links[0]);
+            let feed = await parser.parseURL(link);
             console.log(feed.title);
           
             feed.items.forEach(item => {
-                allRssItems = item;
 
                 eventTitle = item.title;
                 eventDescription = item.content;
@@ -52,11 +48,11 @@ var now = new Date();
                     color : 'green'
                 });
 
-                Event.createEvent(newEvent, function (err, event) {
+               	Event.createEvent(newEvent, function (err, event) {
                     if (err) throw err;
                     console.log('Event created!');
 
-                });
+               	});
 
 
             });
@@ -65,3 +61,4 @@ var now = new Date();
 
     });
 
+return;
